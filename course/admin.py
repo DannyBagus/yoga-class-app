a47e6b1958f99fa08e8acbb412b8c3f4e38d8ac5
DannyBagus <daniel.horn@ik.me>
2025-08-31 19:06:09 +0000

    Story: admin interface showing bookings on course

--- course/admin.py
+++ course/admin.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 
 from .models import Courses, Category, Booking
 
-admin.site.register(Courses)
+class BookingInline(admin.TabularInline):
+    model = Booking
+    # Felder die im Inline angezeigt werden sollen
+    fields = ['user', 'date']
+
+    extra = 0  # Anzahl der zusätzlichen leeren Formulare
+    
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    inlines = [BookingInline]
+
+admin.site.register(Courses, CourseAdmin)
 admin.site.register(Category)
 admin.site.register(Booking)
